69c80ed49a5100ae5a91353d422244a3e3f3c7b0
joaocarlosms <jcmeseverino@gmail.com>
2024-09-30 15:48:11 -0300

    feat: add method delete user by id

--- src/main/java/com/jc/user_dept/controllers/UserController.java
+++ src/main/java/com/jc/user_dept/controllers/UserController.java
@@ -4,8 +4,12 @@ import java.net.ResponseCache;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +32,16 @@ public class UserController {
 	public User findById(@PathVariable Long id) {
 		return repository.findById(id).get();
 	}
+	
+	@PostMapping
+	public User insert(@RequestBody User user) {
+		return repository.save(user);
+	}
+	
+	@DeleteMapping(value = "delete/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+		repository.deleteById(id);
+		return ResponseEntity.ok("User removed with success!");
+	}
+	
 }
